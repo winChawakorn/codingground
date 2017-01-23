65a3dc5bfecfb63896572bd5dbe9bd487c7f6c39
Chawakorn Suphepre <chawakorn.su@ku.th>
2017-01-23 16:00:24 +0700

    Created by Coding Ground Client

---
+++ New Project-20170123/Newfile.rb
@@ -0,0 +1,15 @@
+movies = {
+  Memento: 3,
+  Primer: 4,
+  Ishtar: 1
+}
+ puts "What movie do you want to add?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "What's the rating? (Type a number 0 to 4.)"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been added with a rating of #{rating}."
+  else
+    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
